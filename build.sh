c23b3ac96a78209623f2109183c89955b6b27e6c
franzer <awfulrofl@gmail.com>
2024-06-11 12:31:07 -0600

    update build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,10 @@ RELEASE="$(rpm -E %fedora)"
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
+sh ./1password.sh
 # this installs a package from fedora repos
 rpm-ostree install screen
+rpm-ostree install firefox
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
